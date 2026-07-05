c3df6d3177bc12f9f6d276d12643dd6f5246655c
Reza Fahlevi <reza06117@gmail.com>
2026-07-06 00:00:40 +0700

    Disable CSRF for contact to debug 500

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            '/contact',
+            '/guestbook',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
