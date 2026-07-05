06a26060d8463109ae22e3687c33e9b977479f69
Reza Fahlevi <reza06117@gmail.com>
2026-07-05 22:24:28 +0700

    Fix Vercel Read-Only Filesystem Crash

--- api/index.php
+++ api/index.php
@@ -2,7 +2,29 @@
 
 /**
  * Vercel serverless function entry point for Laravel.
- * This forwards all requests to the standard public/index.php
  */
+
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/bootstrap/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('LOG_CHANNEL=stderr');
 
 require __DIR__ . '/../public/index.php';
